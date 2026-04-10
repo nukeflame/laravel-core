715782fe15123a0c4275323fc9fd4e729eb77c0e
Kennedy Mwanzi <pknuek@gmail.com>
2026-04-10 10:54:04 +0300

    update php version

---
+++ src/CoreServiceProvider.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Nukeflame\LaravelCore;
+
+use Illuminate\Support\ServiceProvider;
+
+class CoreServiceProvider extends ServiceProvider
+{
+    public function register(): void
+    {
+        // Register bindings or singletons here.
+    }
+
+    public function boot(): void
+    {
+        // Boot any package services or publish resources here.
+    }
+}
